3b5479982454c554b526fbc681e7a29a6def05df
Charlotte Waller <charlotteawaller@gmail.com>
2020-02-27 15:39:47 +0000

    Created propositions controller

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -4,4 +4,37 @@ class PropositionsController < ApplicationController
     @propositions = Proposition.all
   end
 
+  def show
+    @proposition = Proposition.new
+  end
+
+  def new
+    @proposition = Proposition.new
+  end
+
+  def create
+    @proposition = Proposition.new(proposition_params)
+    @proposition.save
+  end
+
+  def edit
+    @proposition = Proposition.find(params[:id])
+  end
+
+  def update
+    @proposition = Proposition.find(params[:id])
+  end
+
+  def destroy
+    @proposition = Proposition.find(params[:id])
+    @proposition.destroy
+    redirect_to propositions_path
+  end
+
+  private
+
+  def proposition_params
+    params.require(:proposition).permit(:name, :status)
+  end
+
 end
