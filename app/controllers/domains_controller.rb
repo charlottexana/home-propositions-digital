986da942f7cf8f1397ce6a89d2e353c781f1c0e7
Charlotte Waller <charlotteawaller@gmail.com>
2020-02-27 16:20:13 +0000

    Finished domains controller

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -8,12 +8,15 @@ class DomainsController < ApplicationController
   end
 
   def destroy
+    @domain = Domain.find(domains_params)
+    @domain.destroy
+    redirect_to proposition_domains
   end
 end
 
 private
 
 def domain_params
-
+  params.require(:domain).permit(:authority_score, :age, :link)
 end
 
