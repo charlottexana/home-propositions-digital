c59022c2415d2831dfa086fb17311b4b2821ae09
Charlotte Waller <charlotteawaller@gmail.com>
2020-03-23 09:39:12 +0000

    Fixed weird server shit

--- config/application.rb
+++ config/application.rb
@@ -17,20 +17,6 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 
-url = URI("https://dataforseo-dataforseo-rank-tracker-and-serp-v1.p.rapidapi.com/docs.dataforseo.com/")
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-host"] = 'dataforseo-dataforseo-rank-tracker-and-serp-v1.p.rapidapi.com'
-request["x-rapidapi-key"] = 'b016284af5msh34012b4c1b9c43dp171f75jsn4f6dcf240777'
-
-response = http.request(request)
-puts response.read_body
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
