0cb44096ad10695f10142ae604523df3f982405a
Charlotte Waller <charlotteawaller@gmail.com>
2020-03-30 18:01:08 +0100

    API hell...

---
+++ app/rankwatch.rb
@@ -0,0 +1,19 @@
+require "uri"
+require "net/http"
+
+def rankwatch
+  url = URI("https://apiv2.rankwatch.com//project/detail/json/p_id/c581d4904d184a170420d904bc85791c/s_id/2.00426/")
+
+  https = Net::HTTP.new(url.host, url.port);
+  https.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+  request["Authorization"] = "Basic cnctdjItMzg4NjktMjE5MTM6NmFiRHFUNzZSY1NI"
+
+  response = https.request(request)
+  puts response.read_body
+
+end
+
+helper_method :rankwatch
+
