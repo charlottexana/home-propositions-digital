ca8b61bcacfe1b38ed4c66dde25bdd3d0ad4e12f
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-03 15:22:07 +0100

    Osas got API call working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,57 +6,23 @@
 // require 'net/http'
 // require 'openssl'
 
-
-console.log("hello there!");
-
-
-// window.onload = (function(){getKeyword()});
+document.addEventListener('DOMContentLoaded', function(){
 
 const myKeyword = document.querySelector('#keywords');
-
-const getKeyword = (keyword) => {
-  myKeyword.innerHTML = "";
-};
-
+// debugger
 
 fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom')
-.then(response => response.json())
-.then((data) => {
-console.log(data);
+  .then(response => response.json())
+  .then((data) => {
+    const newRanking = (data.organic.map(position => {
+      return position.title
+    }));
 
-const newRanking = (data.organic[0].title);
-
-myKeyword.insertAdjacentHTML('beforeend', newRanking)});
-
-// const request = require('request');
-
-// const options = { url: 'https://app.zenserp.com/api/v2/search?apikey=YOUR-API-KEY&q=landlord lifeguard&device=desktop&gl=GB&hl=en&location=United Kingdom' };
-
-// function callback(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body);
-//   }
-// }
-
-
-// getKeyword();
-
-// request(options, callback);
-
-// var settings = {
-//   "url": "https://apiv2.rankwatch.com//project/detail/json/p_id/c581d4904d184a170420d904bc85791c/s_id/2.00426/",
-//   "method": "GET",
-//   "timeout": 0,
-//   "headers": {
-//     "Authorization": "Basic cnctdjItMzg4NjktMjE5MTM6NmFiRHFUNzZSY1NI"
-//   },
-// };
-
-
-
-// getKeyword(data);
-// });
-
-// console.log(myKeyword);
-
-// ${location.value}
+  for (var i = 0; i < newRanking.length; i++) {
+      const title = newRanking[i];
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(title ));
+      myKeyword.appendChild(li);
+  }}
+  );
+});
