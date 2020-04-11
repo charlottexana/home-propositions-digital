6fae45cc698a167f49ee396d13f6070713742512
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-11 18:46:59 +0100

    Heroku errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  config.assets.uglifier = { harmony: true }
+  # Added line 26 to try and get Heroku to upload.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
