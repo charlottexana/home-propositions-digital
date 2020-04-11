5928d5bec0820de9fbaaa5495dde72d99d685d85
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-11 18:59:16 +0100

    Changed .rb file to try and get Heroku working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,19 +8,19 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-var myKeyword = document.querySelector('#keywords');
+const myKeyword = document.querySelector('#keywords');
 // debugger
 
 fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom')
   .then(response => response.json())
   .then((data) => {
-    var newRanking = (data.organic.map(position => {
+    const newRanking = (data.organic.map(position => {
       return position.title
     }));
 
   for (var i = 0; i < newRanking.length; i++) {
-      var title = newRanking[i];
-      var li = document.createElement('li');
+      const title = newRanking[i];
+      const li = document.createElement('li');
       li.appendChild(document.createTextNode(title ));
       myKeyword.appendChild(li);
   }}
